da2868d87afc44c6a871414774c27d2b698e963c
Mario <mariosantos88477364484@gmail.com>
2024-01-17 11:20:33 -0300

    Project setup

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/** @type {import("prettier").Config} */
+const config = {
+	useTabs: true,
+	tabWidth: 4,
+	plugins: ["prettier-plugin-tailwindcss"],
+};
+
+export default config;
